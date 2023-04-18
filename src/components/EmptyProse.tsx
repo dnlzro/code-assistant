8f7a49cbd06e69362a58037324283d913bcde19f
Daniel Lazaro <git@dlazaro.ca>
2023-04-17 23:47:34 -0400

    Fix prose width issue

--- src/components/EmptyProse.tsx
+++ src/components/EmptyProse.tsx
@@ -1,6 +1,6 @@
 function EmptyProse() {
   return (
-    <div class="prose !prose-nord px-5">
+    <div class="prose !prose-nord max-w-none px-5">
       <p class="text-nord-4/75">Waiting to receive input.</p>
     </div>
   );
