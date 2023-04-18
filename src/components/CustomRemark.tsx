9a30db93ed95519544705b2633e0db7e87f8ec7a
Daniel Lazaro <git@dlazaro.ca>
2023-04-18 00:22:58 -0400

    Fix prose padding

--- src/components/CustomRemark.tsx
+++ src/components/CustomRemark.tsx
@@ -12,7 +12,7 @@ function CustomRemark({
     <ReactMarkdown
       remarkPlugins={[remarkGFM]}
       className={
-        "prose !prose-nord max-w-none px-5" +
+        "prose !prose-nord max-w-none px-5 " +
         (success
           ? "prose-code:bg-nord-4/10"
           : "prose-p:text-red-300 prose-a:text-red-300 prose-strong:text-red-300 prose-code:bg-red-300/10 prose-code:text-red-100")
